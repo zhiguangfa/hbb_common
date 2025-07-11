5caf726410bfeea8d0f5f994f815693c0381e9b6
zhiguangfa <zhiguangfa@gmail.com>
2025-07-11 09:51:21 +0800

    改为ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["139.199.170.15"];
+pub const RS_PUB_KEY: &str = "cZI+AyaosQI3f8+7Q7Dq+qoDlESEBZMEk8ve8sII978=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
